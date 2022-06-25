ce8cb02e98c2571745913c6920fe1cc2b16bdd41
juanarias2004 <ariasporrasjuandavid@gmail.com>
2022-06-25 08:44:39 -0500

    cambios

--- src/Ciclos.java
+++ src/Ciclos.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class Ciclos {
     public static void main(String[] args) {
+        for (int i = 0; 1 < 3; i++){
+            JOptionPane.showMessageDialog(null, "HOLA MUNDO");
+        }
 
     }
 }
